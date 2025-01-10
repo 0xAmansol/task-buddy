20357b057bb8acd8fcf6d8a2581c13eb1ed1b029
Aman kalal <amankalal206@gmail.com>
2025-01-10 15:35:34 +0530

    Added all fixes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBnKsxV7jejz7sEK9td-T_JmWE8mLjqvqM",
-  authDomain: "taskmanager-30369.firebaseapp.com",
-  projectId: "taskmanager-30369",
-  storageBucket: "taskmanager-30369.appspot.com", // Corrected this line
-  messagingSenderId: "44585153556",
-  appId: "1:44585153556:web:e21ea7733625c21091a301",
-  measurementId: "G-HN2DMT6ZSZ",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
